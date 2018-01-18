37ca873d2ff5237c78eca55c95dcc31d24ff5d42
longhuashen <2853718545@qq.com>
2018-01-18 17:56:07 +0800

    修正错误

--- common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
+++ common/src/main/java/io/netty/util/concurrent/MultithreadEventExecutorGroup.java
@@ -149,7 +149,7 @@ public abstract class MultithreadEventExecutorGroup extends AbstractEventExecuto
 
     @Override
     public EventExecutor next() {
-        //从worker reactor线程组中选择一个reactor线程
+        //从reactor线程组中选择一个reactor线程
         return chooser.next();
     }
 
