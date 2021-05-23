a0c57a4a020dc5461759db93c34e563dcbd6fc0b
huashen <huashen361@gmail.com>
2021-05-23 23:53:37 +0800

    Handler初步解析

--- transport/src/main/java/io/netty/channel/ChannelHandler.java
+++ transport/src/main/java/io/netty/channel/ChannelHandler.java
@@ -182,12 +182,14 @@ public interface ChannelHandler {
     /**
      * Gets called after the {@link ChannelHandler} was added to the actual context and it's ready to handle events.
      */
+    //Handler本身被添加到ChannelPipeline时调用
     void handlerAdded(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * Gets called after the {@link ChannelHandler} was removed from the actual context and it doesn't handle events
      * anymore.
      */
+    //Handler本身被从ChannelPipeline中删除时调用
     void handlerRemoved(ChannelHandlerContext ctx) throws Exception;
 
     /**
@@ -196,6 +198,7 @@ public interface ChannelHandler {
      * @deprecated is part of {@link ChannelInboundHandler}
      */
     @Deprecated
+    //发生异常时调用
     void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception;
 
     /**
